9a912facb4dfacaeae71d8b60f45290f9001b59b
wjj <wjeffreyjohnston@gmail.com>
2020-11-18 10:18:16 -0600

    Added analysis methods

--- model.py
+++ model.py
@@ -10,6 +10,9 @@ import sklearn.svm as skc
 
 import general.utility as u
 import general.rf_models as rfm
+import general.plotting as gpl
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 
 tfk = tf.keras
 tfkl = tf.keras.layers
@@ -446,6 +449,7 @@ class BetaVAE(object):
             rep_reg = tfpl.KLDivergenceRegularizer(prior, weight=beta)
         else:
             rep_reg = None
+            
         if conv:
             rep_layer = tfpl.MultivariateNormTriL(encoded_size,
                                                   activity_regularizer=rep_reg)
@@ -485,6 +489,14 @@ class BetaVAE(object):
                  loss=negloglik):
         self.vae.compile(optimizer, negloglik)
         self.compiled = True
+
+    def fit_sets(self, train_set, eval_set=None, **kwargs):
+        _, train_x = train_set
+        if eval_set is not None:
+            _, eval_x = eval_set
+        else:
+            eval_x = None
+        return self.fit(train_x, eval_x=eval_x, **kwargs)
         
     def fit(self, train_x, train_y=None, eval_x=None, eval_y=None, epochs=15,
             data_generator=None, batch_size=32):
@@ -532,13 +544,58 @@ class InputGenerator(object):
     def rvs(self, n):
         out = self.proc(self.source.rvs(n))
         return out
+
+class HalfMultidimensionalNormal(object):
+
+    def __init__(self, *args, set_partition=None, **kwargs):
+        self.args = args
+        self.kwargs = kwargs
+        self.distr = sts.multivariate_normal(*args, **kwargs)
+        if set_partition is None:
+            out = generate_partition_functions(len(self.distr.mean),
+                                               n_funcs=1)
+            pfs, vecs, offs = out
+            set_partition = pfs[0]
+            self.partition = vecs[0]
+            self.offset = offs[0]
+        else:
+            self.partition = None
+            self.offset = None
+        self.partition_func = set_partition
+
+    def rvs(self, rvs_shape):
+        rvs = self.distr.rvs(rvs_shape)
+        while not np.all(self.partition_func(rvs)):
+            mask = np.logical_not(self.partition_func(rvs))
+            sample = self.distr.rvs(rvs_shape)
+            rvs[mask] = sample[mask]
+        return rvs
+
+    def flip(self):
+        set_part = lambda x: np.logical_not(self.partition_func(x))
+        new = HalfMultidimensionalNormal(*self.args, set_partition=set_part,
+                                         **self.kwargs)
+        new.partition = self.partition
+        new.offset = self.offset
+        return new
+
     
-    
-def classifier_generalization(gen, vae, train_func, train_distrib=None,
+def classifier_generalization(gen, vae, train_func=None, train_distrib=None,
                               test_distrib=None, test_func=None,
                               n_train_samples=2*10**4, n_test_samples=10**4,
-                              classifier=skc.SVC, kernel='linear',
-                              **classifier_params):
+                              classifier=skc.SVC, kernel='linear', n_iters=10,
+                              balance_test=True, shuffle=False,
+                              use_orthogonal=True, **classifier_params):
+    if train_func is None:
+        if use_orthogonal and hasattr(train_distrib, 'partition'):
+            orth_vec = train_distrib.partition
+            orth_off = train_distrib.offset
+            out = generate_partition_functions(gen.input_dim, n_funcs=n_iters,
+                                               orth_vec=orth_vec,
+                                               orth_off=orth_off)
+        else:
+            out = generate_partition_functions(gen.input_dim, n_funcs=n_iters)
+        train_func, _, _ = out            
     if train_distrib is None:
         train_distrib = gen.source_distribution
     if test_distrib is None:
@@ -546,18 +603,31 @@ def classifier_generalization(gen, vae, train_func, train_distrib=None,
     if test_func is None:
         test_func = train_func
 
-    train_samples = train_distrib.rvs(n_train_samples)
-    train_labels = train_func(train_samples)
-    train_rep = vae.get_representation(gen.generator(train_samples))
-    c = classifier(kernel=kernel, **classifier_params)
-    c.fit(train_rep, train_labels)
+    scores = np.zeros(n_iters)
+    chances = np.zeros(n_iters)
+    for i in range(n_iters):
+        train_samples = train_distrib.rvs(n_train_samples)
+        train_labels = train_func[i](train_samples)
+        train_rep = vae.get_representation(gen.generator(train_samples))
+        c = classifier(kernel=kernel, **classifier_params)
+        c.fit(train_rep, train_labels)
 
-    test_samples = test_distrib.rvs(n_test_samples)
-    test_labels = test_func(test_samples)
-    test_rep = vae.get_representation(gen.generator(test_samples))
-    score = c.score(test_rep, test_labels)
-    chance = np.max(np.histogram(test_labels.astype(int))[0]/n_test_samples)
-    return score, chance
+        test_samples = test_distrib.rvs(n_test_samples)
+        test_labels = test_func[i](test_samples)
+        if shuffle:
+            snp.random.shuffle(test_labels)
+        type_balance = np.histogram(test_labels.astype(int), bins=2)[0]
+        if balance_test:
+            weights = np.zeros(len(test_labels))
+            weights[test_labels] = 1/type_balance[1]
+            weights[np.logical_not(test_labels)] = 1/type_balance[0]
+        else:
+            weights = None
+
+        test_rep = vae.get_representation(gen.generator(test_samples))
+        scores[i] = c.score(test_rep, test_labels, sample_weight=weights)
+        chances[i] = np.max(type_balance/n_test_samples)
+    return np.mean(scores), np.mean(chances)
 
 def train_multiple_bvae(dg, betas, layer_spec, n_reps=10, batch_size=32,
                         n_train_samps=10**6, epochs=5, hide_print=False,
@@ -582,9 +652,30 @@ def train_multiple_bvae(dg, betas, layer_spec, n_reps=10, batch_size=32,
             models[i, j] = bvae
     return models, training_history
 
+def train_multiple_models_dims(input_dims, *args, n_train_samps=10**5,
+                               samps_list=False, **kwargs):
+    models = []
+    ths = []
+    for i, inp_d in enumerate(input_dims):
+        if samps_list:
+            nts = n_train_samps[i]
+        m, th = train_multiple_models(*args, input_dim=inp_d,  n_train_samps=nts,
+                                      **kwargs)
+        models.append(m)
+        ths.append(th)
+    return np.array(models), np.array(ths)
+
+def evaluate_multiple_models_dims(dg, models, *args, **kwargs):
+    ps, cs = [], []
+    for m in models:
+        p, c = evaluate_multiple_models(dg, m, *args, **kwargs)
+        ps.append(p)
+        cs.append(c)
+    return np.array(ps), np.array(cs)
+
 def train_multiple_models(dg, model_kinds, layer_spec, n_reps=10, batch_size=32,
                           n_train_samps=10**6, epochs=5, hide_print=False,
-                          input_dim=None):
+                          input_dim=None, **kwargs):
     training_history = np.zeros((len(model_kinds), n_reps), dtype=object)
     models = np.zeros_like(training_history, dtype=object)
     if input_dim is None:
@@ -593,7 +684,7 @@ def train_multiple_models(dg, model_kinds, layer_spec, n_reps=10, batch_size=32,
         for j in range(n_reps):
             train_set = dg.sample_reps(sample_size=n_train_samps)
             eval_set = dg.sample_reps()
-            m = mk(dg.output_dim, layer_spec, input_dim)
+            m = mk(dg.output_dim, layer_spec, input_dim, **kwargs)
             if hide_print:
                 with u.HiddenPrints():
                     th = m.fit_sets(train_set, eval_set=eval_set, epochs=epochs,
@@ -607,15 +698,19 @@ def train_multiple_models(dg, model_kinds, layer_spec, n_reps=10, batch_size=32,
 
 
 def evaluate_multiple_models(dg, models, train_func, test_distributions,
-                             **classifier_args):
+                             train_distributions=None, **classifier_args):
     performance = np.zeros(models.shape + (len(test_distributions),))
     chance = np.zeros_like(performance)
+    if train_distributions is None:
+        train_distributions = (None,)*len(test_distributions)
     for i in range(models.shape[0]):
         for j in range(models.shape[1]):
             bvae = models[i, j]
             for k, td in enumerate(test_distributions):
+                train_d_k = train_distributions[k]
                 out = classifier_generalization(dg, bvae, train_func,
                                                 test_distrib=td,
+                                                train_distrib=train_d_k,
                                                 **classifier_args)
                 performance[i, j, k] = out[0]
                 chance[i, j, k]= out[1]
@@ -637,9 +732,17 @@ def train_and_evaluate_models(dg, betas, layer_spec, train_func,
     out = evaluate_multiple_models(dg, models, train_func, test_distributions,
                                    **classifier_args)
     return out, out2 
-                              
+
+def _model_pca(dg, model, n_dim_red=10**4, **pca_args):
+    distrib_pts = dg.source_distribution.rvs(n_dim_red)
+    distrib_reps = dg.generator(distrib_pts)
+    mod_distrib_reps = model.get_representation(distrib_reps)
+    p = skd.PCA(**pca_args)
+    p.fit(mod_distrib_reps)
+    return p
+
 def plot_diagnostics(dg, model, rs, n_arcs, ax=None, n=1000, dim_red=True,
-                     n_dim_red=10*4, **pca_args):
+                     n_dim_red=10**4, pt_size=1.5, **pca_args):
     if ax is None:
         f, ax = plt.subplots(1, 1)
 
@@ -648,18 +751,14 @@ def plot_diagnostics(dg, model, rs, n_arcs, ax=None, n=1000, dim_red=True,
                    (np.zeros_like(angs),)*(dg.input_dim - 2), axis=1)
     
     if dim_red:
-        distrib_pts = dg.source_distribution.rvs(n_dim_red)
-        distrib_reps = dg.generator(distrib_pts)
-        mod_distrib_reps = model.get_representation(distrib_reps)
-        p = skd.PCA(**pca_args)
-        p.fit(mod_distrib_reps)
-    
+        p = _model_pca(dg, model, n_dim_red=n_dim_red,  **pca_args)
+        
     for r in rs:
         s_reps = dg.generator(r*pts)
         mod_reps = model.get_representation(s_reps)
         if dim_red:
             mod_reps = p.transform(mod_reps)
-        ax.plot(mod_reps[:, 0], mod_reps[:, 1], 'o')
+        ax.plot(mod_reps[:, 0], mod_reps[:, 1], 'o', markersize=pt_size)
 
     if n_arcs > 0:
         skips = int(np.round(n/n_arcs))
@@ -673,3 +772,163 @@ def plot_diagnostics(dg, model, rs, n_arcs, ax=None, n=1000, dim_red=True,
                 mod_reps = p.transform(mod_reps)
             ax.plot(mod_reps[:, 0], mod_reps[:, 1], 'o')
     return ax
+
+def generate_partition_functions(dim, offset_distribution=None, n_funcs=100,
+                                 orth_vec=None, orth_off=None):
+    if orth_vec is not None:
+        orth_vecs = u.get_orthogonal_basis(orth_vec)[:, 1:]
+        rand_inds = np.random.choice(orth_vecs.shape[1], n_funcs)
+        plane_vec = orth_vecs[:, rand_inds].T
+    else:
+        direction = np.random.randn(n_funcs, dim)
+        norms = np.expand_dims(np.sqrt(np.sum(direction**2, axis=1)), 1)
+        plane_vec = direction/norms
+    if offset_distribution is not None:
+        offsets = offset_distribution.rvs(n_funcs)
+    else:
+        offsets = np.zeros(n_funcs)
+    if orth_off is not None:
+        offsets = np.ones(n_funcs)*orth_off
+    funcs = np.zeros(n_funcs, dtype=object)
+    for i in range(n_funcs):
+        funcs[i] = lambda x: np.sum(plane_vec[i:i+1]*x, axis=1) - offsets[i] > 0
+    return funcs, plane_vec, offsets
+
+def get_model_dimensionality(dg, models, cutoff=.95, **pca_args):
+    evr, _ = dg.representation_dimensionality(n_components=cutoff)
+    inp_dim = len(evr)
+    model_dims = np.zeros_like(models, dtype=int)
+    for i, m_i in enumerate(models):
+        for j, m_ij in enumerate(m_i):
+            for k, m_ijk in enumerate(m_ij):
+                p = _model_pca(dg, m_ijk, n_components=cutoff, **pca_args)
+                model_dims[i, j, k] = p.n_components_
+    return model_dims, inp_dim
+
+def test_generalization(dg=None, models_ths=None, train_models_blind=False,
+                        p_c=None, dg_kind=FunctionalDataGenerator,
+                        hide_print=True, est_inp_dim=None,
+                        model_kinds=(SupervisedDisentangler, StandardAE)):
+
+    # train data generator
+    inp_dim = 2
+    out_dim = 30
+
+    noise = .1
+    reg_weight = (0, .1)
+    layers =  (20, 50, 50, 50, 30)
+    epochs = 25
+
+    source_var = 1
+
+    if dg is None:
+        dg = dg_kind(inp_dim, layers, out_dim, 
+                     l2_weight=reg_weight, noise=noise)
+
+        source_distr = sts.multivariate_normal(np.zeros(inp_dim), source_var)
+        dg.fit(source_distribution=source_distr, epochs=epochs)
+    else:
+        source_distr = dg.source_distribution
+
+    # test dimensionality
+    rv, vecs = dg.representation_dimensionality(source_distribution=source_distr)
+    f, ax = plt.subplots(1, 1)
+    ax.plot(rv)
+
+    if train_models_blind:
+        train_d2 = HalfMultidimensionalNormal(np.zeros(inp_dim), 1)
+        test_d2 = train_d2.flip()
+        dg.source_distribution = train_d2
+
+    # train models
+    if est_inp_dim is None:
+        est_inp_dim = inp_dim
+    layer_spec = ((40,), (40,), (25,), (10,))
+    batch_size = 1000
+    epochs = 50
+    n_train_diffs = 6
+    train_samples = np.logspace(4, 6.5, n_train_diffs, dtype=int)
+    input_dims = (est_inp_dim,)*n_train_diffs
+    samps_list = True
+    use_x = train_samples
+    log_x = True
+    
+    n_reps = 5
+
+    if models_ths is None:
+        models, th = train_multiple_models_dims(input_dims, dg, model_kinds,
+                                                layer_spec, n_reps=n_reps, 
+                                                batch_size=batch_size,
+                                                epochs=epochs,
+                                                samps_list=samps_list,
+                                                hide_print=hide_print,
+                                                n_train_samps=train_samples)
+
+    else:
+        models, th = models_ths
+        
+    f, ax = plt.subplots(1, 1)
+    legend_patches = []
+    for i, h_i in enumerate(th):
+        color = None
+        for h_ij in h_i:
+            for h_ijk in h_ij:
+                vl = h_ijk.history['val_loss']
+                l = ax.plot(range(len(vl)), vl, color=color)
+                color = l[0].get_color()
+        lpatch = mpatches.Patch(color=color,
+                                label='N = {}'.format(train_samples[i]))
+        legend_patches.append(lpatch)
+    ax.legend(handles=legend_patches)
+        
+    dims, dg_dim = get_model_dimensionality(dg, models)
+    
+    f, ax = plt.subplots(1, 1)
+    
+    ax.hlines(dg_dim, use_x[0], use_x[-1])
+    for i in range(dims.shape[1]):
+        gpl.plot_trace_werr(use_x, dims[:, i].T, ax=ax, log_x=log_x)
+
+
+    tf = None
+    td_same = None
+    input_dim = None
+
+    train_d2 = HalfMultidimensionalNormal(np.zeros(inp_dim), 1)
+    test_d2 = train_d2.flip()
+
+    train_ds = (None, train_d2)
+    tds = (td_same, test_d2)
+    n_iters = 2
+
+    if p_c is None:
+        p, c = evaluate_multiple_models_dims(dg, models, tf, tds,
+                                             train_distributions=train_ds,
+                                             n_iters=n_iters)
+    else:
+        p, c = p_c
+
+
+    plot_wid = 3
+    n_models = p.shape[1]
+    ref_model = 0
+    tds_labels = ('source', 'half-distribution generalization')
+
+    f = plt.figure(figsize=(plot_wid*(n_models + 1), plot_wid))
+
+    ax_comp = f.add_subplot(1, n_models + 1, n_models + 1)
+    ax_j = None
+    col = None
+    for j in range(n_models):
+        ax_j = f.add_subplot(1, n_models + 1, j + 1, sharey=ax_j)
+    
+        for i in range(len(tds)):
+            p_diff = p[:, j] - p[:, ref_model]
+            l = gpl.plot_trace_werr(use_x, p_diff[..., i].T, ax=ax_comp,
+                                    label=tds_labels[i], points=True,
+                                    log_x=log_x, color=col)
+            col = l[0].get_color()
+            l = gpl.plot_trace_werr(use_x, p[:, j, :, i].T, ax=ax_j, 
+                                    points=True, color=col, log_x=log_x)
+
+    return dg, (models, th), (p, c)
