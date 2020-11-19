c150f1e70d948fb0c13675ab7fb17baf49fb0260
wjj <wjeffreyjohnston@gmail.com>
2020-11-19 15:25:53 -0600

    Added newer, more modular version of multi fitting function
    Changed parameters for multi script

--- model.py
+++ model.py
@@ -852,14 +852,14 @@ def test_generalization(dg=None, models_ths=None, train_models_blind=False,
     layer_spec = ((40,), (40,), (25,), (10,))
     batch_size = 1000
     epochs = 60
-    n_train_diffs = 3
-    train_samples = np.logspace(3.5, 5.5, n_train_diffs, dtype=int)
+    n_train_diffs = 6
+    train_samples = np.logspace(3, 6.5, n_train_diffs, dtype=int)
     input_dims = (est_inp_dim,)*n_train_diffs
     samps_list = True
     use_x = train_samples
     log_x = True
     
-    n_reps = 3
+    n_reps = 6
 
     if models_ths is None:
         models, th = train_multiple_models_dims(input_dims, dg, model_kinds,
@@ -876,12 +876,15 @@ def test_generalization(dg=None, models_ths=None, train_models_blind=False,
         
     f, ax = plt.subplots(1, 1)
     legend_patches = []
+    line_styles = ('-', '--', ':', '-.')
     for i, h_i in enumerate(th):
         color = None
-        for h_ij in h_i:
+        for j, h_ij in enumerate(h_i):
+            line_style = line_styles[j % len(line_styles)]
             for h_ijk in h_ij:
                 vl = h_ijk.history['val_loss']
-                l = ax.plot(range(len(vl)), vl, color=color)
+                l = ax.plot(range(len(vl)), vl, color=color,
+                            linestyle=line_style)
                 color = l[0].get_color()
         lpatch = mpatches.Patch(color=color,
                                 label='N = {}'.format(train_samples[i]))
@@ -925,17 +928,205 @@ def test_generalization(dg=None, models_ths=None, train_models_blind=False,
 
     ax_comp = f.add_subplot(1, n_models + 1, n_models + 1)
     ax_j = None
-    col = None
+
     for j in range(n_models):
         ax_j = f.add_subplot(1, n_models + 1, j + 1, sharey=ax_j)
-    
+        line_style = line_styles[j % len(line_styles)]
         for i in range(len(tds)):
             p_diff = p[:, j] - p[:, ref_model]
-            l = gpl.plot_trace_werr(use_x, p_diff[..., i].T, ax=ax_comp,
-                                    label=tds_labels[i], points=True,
-                                    log_x=log_x, color=col)
-            col = l[0].get_color()
+            if j == 0:
+                label = tds_labels[i]
+            else:
+                label = ''
             l = gpl.plot_trace_werr(use_x, p[:, j, :, i].T, ax=ax_j, 
-                                    points=True, color=col, log_x=log_x)
+                                    points=True, log_x=log_x,
+                                    linestyle=line_style)
+            col = l[0].get_color()
+            l = gpl.plot_trace_werr(use_x, p_diff[..., i].T, ax=ax_comp,
+                                    label=label, points=True,
+                                    log_x=log_x, color=col,
+                                    linestyle=line_style)
+
+    rs = (.1, .2, .5)
+    n_arcs = 1
+
+    n_ds, n_mks, n_reps = models.shape
+    rep_ind = 0
+
+    dim_red = True
+
+    psize = 4
+    f, axs = plt.subplots(n_ds, n_mks, figsize=(n_mks*psize,
+                                                n_ds*psize))
+    
+    for i in range(n_ds):
+        for j in range(n_mks):
+            mod = models[i, j, rep_ind]
+            plot_diagnostics(dg, mod, rs, n_arcs, ax=axs[i, j],
+                             dim_red=dim_red)
+
+    return dg, (models, th), (p, c)
+
+def plot_representation_dimensionality(dg, source_distr=None, ax=None):
+    if ax is None:
+        f, ax = plt.subplots(1, 1)
+    rv, vecs = dg.representation_dimensionality(source_distribution=source_distr)
+    ax.plot(rv)
+    return ax
+
+def plot_model_dimensionality(dg, models, use_x, ax=None, log_x=True):
+    if ax is None:
+        f, ax = plt.subplots(1, 1)
+
+    dims, dg_dim = get_model_dimensionality(dg, models)
+    ax.hlines(dg_dim, use_x[0], use_x[-1])
+    for i in range(dims.shape[1]):
+        gpl.plot_trace_werr(use_x, dims[:, i].T, ax=ax, log_x=log_x)
+    return ax
+
+def plot_training_progress(th, ax=None, line_styles=('-', '--', ':', '-.')):
+    if ax is None:
+        f, ax = plt.subplots(1, 1)
+    legend_patches = []
+    for i, h_i in enumerate(th):
+        color = None
+        for j, h_ij in enumerate(h_i):
+            line_style = line_styles[j % len(line_styles)]
+            for h_ijk in h_ij:
+                vl = h_ijk.history['val_loss']
+                l = ax.plot(range(len(vl)), vl, color=color,
+                            linestyle=line_style)
+                color = l[0].get_color()
+        lpatch = mpatches.Patch(color=color,
+                                label='N = {}'.format(train_samples[i]))
+        legend_patches.append(lpatch)
+    ax.legend(handles=legend_patches)
+    return ax
+
+def plot_generalization_performance(use_x, p, plot_wid=3, ref_model=0,
+                                    tds_labels=None, log_x=True,
+                                    line_styles=('-', '--', ':', '-.')):
+    if tds_labels is None:
+        tds_labels = ('source', 'half-distribution generalization')
+
+    n_models = p.shape[1]
+    f = plt.figure(figsize=(plot_wid*(n_models + 1), plot_wid))
+    ax_comp = f.add_subplot(1, n_models + 1, n_models + 1)
+    ax_j = None
+
+    for j in range(n_models):
+        ax_j = f.add_subplot(1, n_models + 1, j + 1, sharey=ax_j)
+        line_style = line_styles[j % len(line_styles)]
+        for i in range(len(tds)):
+            p_diff = p[:, j] - p[:, ref_model]
+            if j == 0:
+                label = tds_labels[i]
+            else:
+                label = ''
+            l = gpl.plot_trace_werr(use_x, p[:, j, :, i].T, ax=ax_j, 
+                                    points=True, log_x=log_x,
+                                    linestyle=line_style)
+            col = l[0].get_color()
+            l = gpl.plot_trace_werr(use_x, p_diff[..., i].T, ax=ax_comp,
+                                    label=label, points=True,
+                                    log_x=log_x, color=col,
+                                    linestyle=line_style)
+    return f
+
+def plot_model_manifolds(dg, models, rs=(.1, .2, .5), n_arcs=1, rep_ind=0,
+                         dim_red=True, psize=4):
+    f, axs = plt.subplots(n_ds, n_mks, figsize=(n_mks*psize,
+                                                n_ds*psize))
+    for i in range(n_ds):
+        for j in range(n_mks):
+            mod = models[i, j, rep_ind]
+            plot_diagnostics(dg, mod, rs, n_arcs, ax=axs[i, j],
+                                dim_red=dim_red)
+    return f
+
+def test_generalization_new(dg=None, models_ths=None, train_models_blind=False,
+                            p_c=None, dg_kind=FunctionalDataGenerator,
+                            dg_args=None, dg_kwargs=None, dg_source_var=1,
+                            dg_train_epochs=25, models_args=None,
+                            models_kwargs=None, models_log_x=True,
+                            use_samples_x=True, models_n_diffs=6,
+                            hide_print=True, est_inp_dim=None,
+                            eval_n_iters=2,
+                            use_mp=False,
+                            model_kinds=(SupervisedDisentangler, StandardAE)):
+    # train data generator
+    if dg_args is None:
+        inp_dim = 2
+        out_dim = 30
+        layers =  (20, 50, 50, 50, 30)
+        dg_args = (inp_dim, layers, out_dim)
+    if dg_kwargs is None:
+        noise = .1
+        reg_weight = (0, .1)
+        dg_kwargs = {'noise':noise, 'l2_weight':reg_weight}
+    
+    if dg is None:
+        dg = dg_kind(*dg_args, **dg_kwargs)
+
+        source_distr = sts.multivariate_normal(np.zeros(inp_dim), dg_source_var)
+        dg.fit(source_distribution=source_distr, epochs=dg_train_epochs,
+               use_multiprocessing=use_mp)
+    else:
+        source_distr = dg.source_distribution
+
+    # test dimensionality
+    plot_representation_dimensionality(dg, source_distr=source_distr)
+
+    if train_models_blind:
+        train_d2 = HalfMultidimensionalNormal(np.zeros(inp_dim), dg_source_var)
+        test_d2 = train_d2.flip()
+        dg.source_distribution = train_d2
+
+    # train models
+    if models_args is None:
+        if est_inp_dim is None:
+            est_inp_dim = inp_dim
+        input_dims = (est_inp_dim,)*models_n_diffs
+        layer_spec = ((40,), (40,), (25,), (10,))
+        models_args = (input_dims, dg, model_kinds, layer_spec)
+    if models_kwargs is None:
+        batch_size = 1000
+        epochs = 60
+        train_samples = np.logspace(3, 6.5, models_n_diffs, dtype=int)
+        samps_list = True
+        n_reps = 3 
+        models_kwargs = {'batch_size':batch_size, 'epochs':epochs,
+                         'samps_list':samps_list, 'n_train_samps':train_samples,
+                         'use_mp':use_mp, 'hide_print':hide_print,
+                         'n_reps':n_reps}
+    if use_samples_x:
+        use_x = models_kwargs['train_samples']
+    else:
+        use_x = models_args[0]
+    
+    if models_ths is None:
+        models, th = train_multiple_models_dims(*models_args, **models_kwargs)
+    else:
+        models, th = models_ths
+        
+    plot_training_progress(th)    
+    plot_model_dimensionality(dg, models, use_x, log_x=model_log_x)
+
+
+    if train_test_distrs is None:
+        train_ds = (None, HalfMultidimensionalNormal(np.zeros(inp_dim), 1))
+        test_d2 = (None, train_d2.flip())
+    else:
+        train_ds, test_ds = train_test_distr
+
+    if p_c is None:
+        p, c = evaluate_multiple_models_dims(dg, models, None, test_ds,
+                                             train_distributions=train_ds,
+                                             n_iters=eval_n_iters)
+    else:
+        p, c = p_c
+
+    plot_generalization_performance(use_x, p, log_x=models_log_x)
+    plot_model_manifolds(dg, models)
 
     return dg, (models, th), (p, c)
--- multi_bvae_script.py
+++ multi_bvae_script.py
@@ -21,15 +21,15 @@ if __name__ == '__main__':
     parser = create_parser()
     args = parser.parse_args()
 
-    betas = (0, 1, 2, 4, 5)
+    betas = (0, 1, 2, 4, 5, 10, 20, 50)
     model_kinds = list(ft.partial(dm.BetaVAE, beta=b) for b in betas)
-    est_inp_dim = 5
+    est_inp_dim = None
 
     if args.data_generator is not None:
         dg_p = pickle.load(args.data_generator)
     else:
         dg = None
-
+        
     use_mp = not args.no_multiprocessing
     out = dm.test_generalization(dg=dg, est_inp_dim=est_inp_dim,
                                  model_kinds=model_kinds, use_mp=use_mp)
