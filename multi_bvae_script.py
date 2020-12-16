c6977ed87a37758afc3ea91601aac63fcc9af082
wjj <wjeffreyjohnston@gmail.com>
2020-12-16 10:16:32 -0600

    Updated bvae script to have same features as partition script

--- multi_bvae_script.py
+++ multi_bvae_script.py
@@ -20,8 +20,10 @@ def create_parser():
                         help='file with saved data generator to use')
     parser.add_argument('-l', '--latent_dims', default=None, type=int,
                         help='number of dimensions to use in latent layer')
+    parser.add_argument('-i', '--input_dims', default=2, type=int,
+                        help='true number of dimensions in input')
     parser.add_argument('-b', '--betas', nargs='*', type=float,
-                        help='betas to fit models for')
+                        help='beta weight to use')
     parser.add_argument('-c', '--save_datagenerator', default=None,
                         type=str, help='path to save data generator at')
     parser.add_argument('-n', '--n_reps', default=5,
@@ -29,28 +31,50 @@ def create_parser():
     parser.add_argument('-t', '--n_train_diffs', default=6, type=int,
                         help='number of different training data sample sizes '
                         'to use')
+    parser.add_argument('--n_train_bounds', nargs=2, default=(2, 6.5),
+                        type=float, help='order of magnitudes for range to '
+                        'sample training differences from within (using '
+                        'logspace)')
+    parser.add_argument('-m', '--no_models', default=False, action='store_true',
+                        help='do not store tensorflow models')
+    parser.add_argument('--test', default=False, action='store_true',
+                        help='run minimal code to test that this script works')
     return parser
 
 if __name__ == '__main__':
     parser = create_parser()
     args = parser.parse_args()
 
-    betas = args.betas
-    model_kinds = list(ft.partial(dd.BetaVAE, beta=b) for b in betas)
+    true_inp_dim = args.input_dims
     est_inp_dim = args.latent_dims
-    n_reps = args.n_reps
-    n_train_diffs = args.n_train_diffs
+    if args.test:
+        n_reps = 1
+        n_train_diffs = 1
+        dg_train_epochs = 1
+    else:
+        n_reps = args.n_reps
+        n_train_diffs = args.n_train_diffs
+        dg_train_epochs = 25
 
+    save_tf_models = not args.no_models
     if args.data_generator is not None:
         dg_use = dg.FunctionalDataGenerator.load(args.data_generator)
+        inp_dim = dg_use.input_dim
     else:
         dg_use = None
 
-    use_mp = not args.no_multiprocessing
-    out = dc.test_generalization(dg=dg_use, est_inp_dim=est_inp_dim,
-                                 n_reps=n_reps, model_kinds=model_kinds,
-                                 use_mp=use_mp, n_train_diffs=n_train_diffs)
-    dg, (models, th), (p, c) = out
+    betas = args.betas
+    model_kinds = list(ft.partial(dd.BetaVAE, beta=b) for b in betas)
 
-    da.save_generalization_output(args.output_folder, dg, models, th, p, c)
-    
+    use_mp = not args.no_multiprocessing
+    out = dc.test_generalization_new(dg=dg_use, est_inp_dim=est_inp_dim,
+                                     inp_dim=true_inp_dim,
+                                     dg_train_epochs=dg_train_epochs,
+                                     n_reps=n_reps, model_kinds=model_kinds,
+                                     use_mp=use_mp, models_n_diffs=n_train_diffs,
+                                     models_n_bounds=args.n_train_bounds)
+    dg, (models, th), (p, c), (lrs, scrs, sims) = out
+
+    da.save_generalization_output(args.output_folder, dg, models, th, p, c,
+                                  lrs, (scrs, sims),
+                                  save_tf_models=save_tf_models)
