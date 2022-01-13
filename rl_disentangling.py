e1afad66137c437563d48c436be6dd07c29758bb
wjj <wjeffreyjohnston@gmail.com>
2022-01-13 12:13:15 -0500

    Even more imports

--- rl_disentangling.py
+++ rl_disentangling.py
@@ -3,14 +3,21 @@ import tensorflow_probability as tfp
 import tf_agents as tfa
 import reverb
 
-from tf_agents.specs import tensor_spec
-from tf_agents.replay_buffers import reverb_replay_buffer
-from tf_agents.replay_buffers import reverb_utils
+from tf_agents.agents.dqn import dqn_agent
+from tf_agents.drivers import py_driver
+from tf_agents.environments import suite_gym
+from tf_agents.environments import tf_py_environment
+from tf_agents.eval import metric_utils
+from tf_agents.metrics import tf_metrics
+from tf_agents.networks import sequential
 from tf_agents.policies import py_tf_eager_policy
 from tf_agents.policies import random_tf_policy
+from tf_agents.replay_buffers import reverb_replay_buffer
+from tf_agents.replay_buffers import reverb_utils
 from tf_agents.trajectories import trajectory
+from tf_agents.specs import tensor_spec
+from tf_agents.utils import common
 
-import collections as c
 import general.utility as u
 import general.plotting as gpl
 import disentangled.aux as da
